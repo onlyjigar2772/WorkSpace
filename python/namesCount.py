cfb3b9e131cd7c866759a89d00db1c4e3abb35c2
Jigar <onlyjigar2772@gmail.com>
2015-04-07 11:27:00 +0530

    sendMail.py used to send emails
    Cannot verify as it can be a proxy issue.
    
    http://www.linuxjournal.com/content/python-scripts-replacement-bash-utility-scripts?page=0,1

---
+++ python/namesCount.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+
+if __name__ == "__main__":
+    names = {}
+    # sys.stdin is a file object.
+    for name in sys.stdin.readlines():
+        name = name.strip()
+        if name in names:
+            names[name] += 1
+        else:
+            names[name] = 1
+
+#printing the new dic
+for name, count in names.iteritems():
+    sys.stdout.write("%d\t%s\n" % (count, name))
+
---
+++ python/sendMail.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import smtplib
+import sys
+
+GMAIL_SMTP_SERVER = "smtp.gmail.com"
+GMAIL_SMTP_PORT = 587
+
+GMAIL_EMAIL = "onlyjigar2772@gmail.com"
+GMAIL_PASSWORD = ""
+
+def initialize_smtp_server():
+    smtpserver = smtplib.SMTP(GMAIL_SMTP_SERVER, GMAIL_SMTP_PORT)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo()
+    smtpserver.login(GMAIL_EMAIL, GMAIL_PASSWORD)
+    return smtpserver
+
+def send_thank_you_mail():
+    to_email = 'jigar.shah\@wipro.com'
+    email = 'jigar.shah\@wipro.com'
+    from_email = GMAIL_EMAIL
+    subj = "Hello"
+    header = "To:%s\nFrom:%s\nSubject:%s \n" % (to_email,
+            from_email, subj)
+    # Hard-coded templates are not best practice.
+    msg_body = """
+    Hi %s,
+
+    Thank you very much for your repeated comments on our service.
+    The interaction is much appreciated.
+
+    Thank You.""" % email
+    content = header + "\n" + msg_body
+    smtpserver = initialize_smtp_server()
+    smtpserver.sendmail(from_email, to_email, content)
+    smtpserver.close()
+    
+if __name__ == "__main__":
+    send_thank_you_mail()
