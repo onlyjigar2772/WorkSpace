4214eb11f10964e9d86ea48c853ea308386ad215
Jigar Shah <onlyjigar2772@gmail.com>
2015-04-13 17:03:27 +0530

    Example - fibo.py

---
+++ python/fibo1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+fibs = [0,1]
+'''
+for i in range(8):
+    fibs.append(fibs[-2] + fibs[-1])
+'''
+
+num = input('How many Fibonacci numbers do you want? ')
+for i in range(num-2):
+    fibs.append(fibs[-2] + fibs[-1])
+print fibs
