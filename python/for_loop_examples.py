f8dd0901693f8507a5c8defc5c0c2eba6858d726
Jigar Shah <onlyjigar2772@gmail.com>
2015-04-29 22:29:05 +0530

    Trying inbuilt commit

--- python/for_loop_examples.py
+++ python/for_loop_examples.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#Print 'Hi' 10 times
+# Print 'Hi' 10 times
 
 for i in range(10):
     print("Hi")
@@ -8,36 +8,37 @@ for i in range(10):
 for i in range(5):
     print("Hello")
     print("There")
-	
+
 for i in range(10):
     print(i+1)
-	
+
 for i in range(10, 0, -1):
     print(i)
-	
+
 for i in range(3):
     print("a")
     for j in range(3):
-	    print("b")
-		
-#What is teh value of a
+        print("b")
+
+# What is the value of a
 a = 0
 for i in range(10):
-    a = a + 1
+    a += 1
 print(a)
 
 # What is the value of a?
 a = 0
 for i in range(10):
-    a = a + 1
+    a += 1
 for j in range(10):
-    a = a + 1
+    a += 1
 print(a)
 
 # What is the value of a?
 a = 0
 for i in range(10):
-    a = a + 1
+    a += 1
     for j in range(10):
-        a = a + 1
-print(a) 
+        a += 1
+print(a)
+
---
+++ python/while_loop_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+__author__ = 'Jigar'
+
+# While loop example.
+
+buttcrack = 5
+
+while buttcrack < 10:
+    print(buttcrack)
+    buttcrack += 1
+
+
+
